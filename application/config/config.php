b6faabc3ce9f8e91b7cbe9f1e72edd5b9a153189
PatPunyanuch <124332846+PatPunyanuch@users.noreply.github.com>
2023-02-06 10:15:38 +0700

    Update
    update config

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = 'http://localhost/Qgateweb';
+// $config['base_url'] = 'http://localhost/Qgateweb';
+$config['base_url'] = 'http://192.168.161.214/QGateweb/';
 
 
 $config['index_page'] = '';
@@ -51,7 +52,7 @@ $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = '/tmp';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
